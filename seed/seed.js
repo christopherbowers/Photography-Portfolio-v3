a715a09bbb4a9e6d3aa36c4e29b3c5ddb0576339
Christopher Bowers <c_bowers@me.com>
2021-12-16 15:46:02 -0500

    Added seed script to scripts

--- seed/seed.js
+++ seed/seed.js
@@ -4,23 +4,29 @@ const Project = require('../models/')
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 const main = async () => {
-    const projects = {
-      title: "Constant Illuminations",
-      image: [
-        {
-          title: "Lindsay",
-          year: "2008"
-        }
-      ]
-    }
+  const projects = {
+    title: "Constant Illuminations",
+    image: [
+      {
+        title: "Lindsay",
+        year: "2008"
+      }
+    ]
+  }
 
-    await Project.insertMany(projects)
-    console.log("Created projects")
+  await Project.insertMany(projects)
+  console.log("Created projects")
+}
+
+const dropDbs = async () => {
+  await Project.collection.drop()
+  console.log('Dropped DBs')
 }
 
 const run = async () => {
-    await main()
-    db.close()
+  await dropDbs()
+  await main()
+  db.close()
 }
 
 run()
