2eb30c15c13ebfae286677167ddc2637e3d4ce41
Christopher Bowers <c_bowers@me.com>
2021-12-17 15:25:51 -0500

    Fixed routes not updating state

--- client/src/components/ProjectPage.js
+++ client/src/components/ProjectPage.js
@@ -4,25 +4,37 @@ import axios from 'axios'
 
 const ProjectPage = () => {
   const { slug } = useParams()
-
+  
+  const [isLoading, setLoading] = useState(true);
   const [project, setProject] = useState({})
   
   useEffect(() => {
-    getProject()
-  }, [])
-
-  const getProject = async () => {
-    const res = await axios.get('http://localhost:3001/api/projects/' + slug )
-    setProject(res.data.project)
-  }
-
-  console.log(project.image)
-
-  return (
+    axios.get('http://localhost:3001/api/projects/' + slug ).then(res => {
+      setProject(res.data.project)
+      document.title = res.data.project.title
+      setLoading(false)
+    })
+  }, [slug])
   
+if (isLoading) {
+  return <div class="lds-dual-ring"></div>
+}
+
+return (
+  
+    <div>
     <h2>{project.title}</h2>
-    
-    
+      <div>
+      { 
+        project.image.map((image, index) => (
+        <div key={index}>
+          <img src={image.image_url} alt='' />
+          <p><strong>Title:</strong> {image.title}</p>
+        </div>
+        ))
+      }
+      </div>
+    </div>   
 
   
   )
