8e09719956e1a014b53c0a51536dde10c668b0ea
Christopher Bowers <c_bowers@me.com>
2021-12-20 13:43:52 -0500

    Added image upload code but getting 404

--- client/src/components/AddImages.js
+++ client/src/components/AddImages.js
@@ -1,12 +1,40 @@
-// import { useNavigate } from 'react-router-dom'
-// import { useState } from 'react'
-// import axios from 'axios'
+import { useState } from 'react'
+import axios from 'axios'
 
-export default function AddImages(props) {
+export default function AddImages() {
+
+async function postImage({image, description}) {
+  const formData = new FormData();
+  formData.append("image", image)
+  formData.append("description", description)
+
+  const result = await axios.post('/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+  console.log(result)
+  return result.data
+}
+
+  
+  
+  const [file, setFile] = useState()
+  const [description, setDescription] = useState("")
+  const [images, setImages] = useState([])
+
+  const submit = async event => {
+    event.preventDefault()
+    const result = await postImage({image: file})
+    setImages([result.image, ...images])
+  }
+
+  const fileSelected = event => {
+    const file = event.target.files[0]
+		setFile(file)
+	}
+
   
   return (
+  
     <div className="form-container">
-      <form onSubmit={ props.handleSubmitImage } >
+      {/*<form onSubmit={ props.handleSubmitImage } >
         <label>Add Image: </label>
         <select onChange={ props.handleChange } name="title">
               <option>Select Project:</option>
@@ -16,16 +44,27 @@ export default function AddImages(props) {
             ))
           }
         </select>
-        {/*<input type="text" name="title" onChange={ props.handleChange } placeholder="Project Title" />*/}
+       
         <input type="text" name="image_title" onChange={ props.handleChange } placeholder="Image Title" />
         <input type="text" name="year" onChange={ props.handleChange } placeholder="Year"  />
         
         
-        {/*<input type="file" name="file" onChange={ props.handleChange } />*/}
+        <input type="file" name="file" onChange={ props.handleChange } />
         
         
         <button type="submit">Add</button>
+      </form>*/}
+      
+      
+      <form onSubmit={submit}>
+        <input onChange={fileSelected} type="file" accept="image/*"></input>
+        <input value={description} onChange={e => setDescription(e.target.value)} type="text"></input>
+        <button type="submit">Add</button>
       </form>
+       
+      
+      
+      
     </div>
   )
 }
--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const db = require('./db')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
+// const multer = require('multer')
+// const express = require('express')
+const slugify = require('slugify')
+const fs = require('fs')
+const util = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+
+
+
+
+
 const PORT = process.env.PORT || 3001
 
 const app = express()
@@ -16,7 +27,51 @@ app.use(cors())
 
 app.use('/api', routes)
 
+const multer = require('multer')
+
+let storage = multer.diskStorage(
+  {
+    destination: 'uploads/',
+    filename: function ( req, file, cb ) {
+      cb( null, slugify(file.originalname, {remove: /[*+~()'"!:@]/g}).toLowerCase());
+    }
+  }
+)
+
+const upload = multer( { storage: storage } );
+
+const { uploadFile, getFileStream } = require('./controllers/s3')
+
+
+app.post('/images', upload.single('image'), async (req, res) => {
+  
+  const file = req.file
+  console.log(file)
+
+  const result = await uploadFile(file)
+  await unlinkFile(file.path)
+  console.log(result)
+  const description = req.body.description
+  console.log(description)
+  res.send({imagePath: `/images/${result.Key}`})
+  
+})
+
+app.get('/images/:key', (req, res) => {
+  console.log(req.params)
+  const key = req.params.key
+  const readStream = getFileStream(key)
+
+  readStream.pipe(res)
+})
+
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.listen(PORT, () => console.log(`Express Listening on port: ${PORT}`))
+
+/////////////////////////////////////////////////////////////////////////
+
+
+
+ 
