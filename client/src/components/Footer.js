6cca5c08f6760087fba331d22645ef8b20c78f8a
Christopher Bowers <c_bowers@me.com>
2021-12-20 17:01:10 -0500

    Added login toggle to show dashboard

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,8 @@
-import { NavLink } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function Footer() {
   return (
     <footer>
-      <ul>
-        <li><NavLink to="/dashboard">Dash Board</NavLink></li>
-        <li><NavLink to="/">Home</NavLink></li>
-      </ul>
+      <Link to="/">Home</Link>
     </footer>
   )
 }
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,17 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import DashBoardLink from './DashBoardLink'
 
 let name = 'Christopher Bowers'
 
-export default function Header() {
+export default function Header(props) {
+
   return (
     <header>
       <h1>
         <Link to="/">{ name }</Link>: Lens Based Media
       </h1>
+      <DashBoardLink isLoggedIn={ props.isLoggedIn } />
     </header>
   )
 }
