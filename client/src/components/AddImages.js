db9897c5d2740a4d945b3b1f40dff7d427b9cd49
Christopher Bowers <c_bowers@me.com>
2021-12-21 12:02:47 -0500

    Image upload and get complete

--- client/src/components/AddImages.js
+++ client/src/components/AddImages.js
@@ -5,24 +5,19 @@ import slugify from 'react-slugify'
 export default function AddImages(props) {
 
 
-
-async function postImage({image}) {
-  const formData = new FormData();
-  formData.append("image", image)
+  async function postImage({image}) {
+    const formData = new FormData();
+    formData.append("image", image)
   
-  const result = await axios.post('/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
-//   console.log(result)
+    const result = await axios.post('/images', formData, { headers: {'Content-Type': 'multipart/form-data'}})
+    return result.data
+  }
 
-  return result.data
-}
-
-// 
-  
   const [file, setFile] = useState()
   const [imageTitle, setImageTitle] = useState('')
   const [year, setYear] = useState('')
   const [images, setImages] = useState([])
-  const [imagePath, setImagePath] = useState('')
+
 
   const fileSelected = event => {
     const file = event.target.files[0]
@@ -31,55 +26,47 @@ async function postImage({image}) {
 
   const submit = async (e) => {
     e.preventDefault()
-    console.log(file)
+//     console.log(file)
 
     const result = await postImage({image: file})
     setImages([result.image, ...images])
-    slugify(file.name)
     
+    const projectTitle = encodeURI(props.inputValue.title)
     
+//     console.log()
     
-   
     await axios
-      .put(`http://localhost:3001/api/projects/Stunts`, {
+      .put(`http://localhost:3001/api/projects/${projectTitle}`, {
         image_title: imageTitle,
         year: year,  
-        image_url: '/images/' + slugify(file.name)
-      })
-    
+        image_url: '/images/' + (file.name).replaceAll(/[\s*+~()'"!:@]/g, '-')
+    })
   }
 
-
-
-
-  
   return (
   
     <div className="form-container">
 
       <form onSubmit={ submit } >
-      
+
         <label>Add Image: </label>
         <select onChange={ props.handleChange } name="title">
-              <option>Select Project:</option>
+          <option>Select Project:</option>
           {
             props.projects.map((project) => (
               <option key={ project._id } >{ project.title }</option>
             ))
           }
         </select>
-        
+
         <input onChange={ fileSelected } type="file" accept="image/*"></input>
-        
-        <input value={ imageTitle } onChange={e => setImageTitle(e.target.value)} type="text" placeholder="Image Title" />
+        <input value={ imageTitle } onChange={e => setImageTitle(e.target.value) } type="text" placeholder="Image Title" />
         <input value={ year } onChange={e => setYear(e.target.value)} type="text" placeholder="Year" />
         
         <button type="submit">Add</button>
       </form>
-       
-      
-      <img src="/images/aquaice7ty.jpg" />
       
     </div>
+    
   )
 }
