866a12b63b2287d980dfe1a7820e58f715d1b2dd
Christopher Bowers <c_bowers@me.com>
2022-01-27 20:59:22 -0500

    Refactored App, moved axios to nav, toggle login/dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -2,66 +2,31 @@ import { useState, useEffect, Suspense, lazy } from 'react'
 import { Route, Routes, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import './App.css'
-import Header from './components/Header'
 import Home from './pages/Home'
-import Nav from './components/Nav'
-import Footer from './components/Footer'
 import ProjectPage from './pages/ProjectPage'
-import LayoutsWithNavbar from './components/LayoutsWithNavbar'
 
-const Login = lazy(() => import ('./pages/Login'))
-const Dashboard = lazy(() => import ('./pages/Dashboard'))
+const LayoutsWithNavbar = lazy(() => import('./components/LayoutsWithNavbar'))
+
+const Login = lazy(() => import('./pages/Login'))
+const Dashboard = lazy(() => import('./pages/Dashboard'))
 
 function App() {
   // eslint-disable-next-line
   const navigate = useNavigate()
 
-  const [projects, setProjects] = useState([])
-
-  const getProjects = async () => {
-    const res = await axios.get('/api/projects')
-    setProjects(res.data.projects)
-    document.title = 'Christopher Bowers'
-  }
-
-  useEffect(() => {
-    getProjects()
-  }, [])
-
-
-  const showNav = () => {
-    if (window.location.pathname === '/dashboard'  || window.location.pathname === '/login') {
-      return null
-    } else {
-     return ( <Nav projects={ projects }/> )
-    }
-  }
-
-  const showHeader = () => {
-    if (window.location.pathname === '/dashboard' || window.location.pathname === '/login') {
-      return null
-    } else {
-     return ( <Header /> )
-    }
-  }
-
   return (
     <div className="App">
-        <Suspense fallback={<>loading...</>}>
-          <Routes>
-
-          <Route path="/" element={ <LayoutsWithNavbar projects={ projects } /> }>
-              <Route path="/" element={ <Home /> } />
-              <Route path="/projects/:slug" element={ <ProjectPage /> }/>
+      <Suspense fallback={<>loading...</>}>
+        <Routes>
+          <Route path="/" element={ <LayoutsWithNavbar /> }>
+            <Route path="/" element={ <Home /> } />
+            <Route path="/projects/:slug" element={ <ProjectPage /> }/>
           </Route>
-
           <Route path="/login" element={ <Login /> }/>
           <Route path="/dashboard" element={ <Dashboard /> } />
-
-          </Routes>
-        </Suspense>
+        </Routes>
+      </Suspense>
     </div>
-
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,14 +2,20 @@ import { Link } from 'react-router-dom'
 
 let name = 'Christopher Bowers'
 
-export default function Header(props) {
+export default function Header() {
+
+const userInfo = localStorage.getItem('userInfo')
+? JSON.parse(localStorage.getItem('userInfo'))
+: ''
+
+
 
   return (
     <header>
       <h1>
         <Link to="/">{ name }</Link>: Lens Based Media
       </h1>
-      <Link to="/dashboard">Dashboard</Link>
+      {userInfo ? <Link to="/dashboard">Dashboard</Link> : <Link to="/login" style={{color: 'black'}}>Login</Link>}
     </header>
   )
 }
