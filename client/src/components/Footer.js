ecf0ed05eeccff277ac723cdcc220c33259cbb89
Christopher Bowers <c_bowers@me.com>
2022-05-10 14:54:19 +0000

    Updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,11 +1,11 @@
-export default function Footer(props) {
+export default function Footer() {
+
+  const year = new Date().getFullYear()
 
   return (
     <footer>
-      <p>This site is built with the <a href="https://www.mongodb.com">M</a><a href="http://expressjs.com">E</a><a href="https://reactjs.org">R</a><a href="https://node.js">N</a> stack and hosted on <a href="https://heroku.com">Heroku</a>. The source code is hosted on <a href="https://github.com/christopherbowers/Photography-Portfolio-v2">Github</a>.</p>
-      <div>
-      </div>
+      <p>Designed &amp; Built by Christopher Bowers</p>
+      <p>Copyright &copy; 2005&ndash;{year}</p>
     </footer>
   )
 }
-
