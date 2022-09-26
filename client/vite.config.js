c37461496a627451ea80465cb05715504b4d06b6
christopherbowers <c_bowers@me.com>
2022-09-26 16:01:26 -0400

    vite config
    Fixed vite conf for dev
    
    vercel rewrites
    
    vercel heroku url
    
    vercel json rewrites order change
    
    removed comma
    
    /images rewrite
    
    images vercel fix
    
    images
    
    images

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,22 +1,51 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const defaultConfig = {
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-         target: 'http://localhost:3001',
-         changeOrigin: true,
-         secure: false,
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
        },
       '/images': {
-         target: 'http://localhost:3001/images',
-         changeOrigin: true,
-         secure: false,
-         rewrite: (path) => path.replace(/^\/images/, '')
+          target: 'http://localhost:3001/images',
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/images/, '')
        },
+    }
+  }
+}
+
+export default defineConfig(({ command }) => {
+  if (command === 'serve') {
+    return {
+      // dev specific config
+      ...defaultConfig
+    }
+  } else {
+    // command === 'build'
+    return {
+      // build specific config
+      ...defaultConfig,
+      server: {
+        proxy: {
+          '/api': {
+            target: 'http://bowers-portfolio.herokuapp.com/api/',
+            changeOrigin: true,
+            secure: false,
+           },
+          '/images': {
+            target: 'http://bowers-portfolio.herokuapp.com/api/images/',
+            changeOrigin: true,
+            secure: false,
+            rewrite: (path) => path.replace(/^\/images/, '')
+           },
+        }
+      }
     }
   }
 })
