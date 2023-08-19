4d6af8458a88919946c0f41a7991c0a28017c9ff
christopherbowers <c_bowers@me.com>
2023-08-19 08:48:16 -0400

    Hardened Home with random images

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,41 +1,40 @@
-import { useEffect, useState } from 'react'
-import styles from './Home.module.scss'
-import Loading from '../components/Loading'
+import { useEffect, useState } from 'react';
+import styles from './Home.module.scss';
+import Loading from '../components/Loading';
 
 export default function Home() {
-
-  const [images, setImages] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   const getImages = async () => {
     await fetch('/api/images')
-    .then(res => res.json())
-    .then(data => {
-      setImages(data.images)
-      setLoading(false)
-    })
-    .catch( error => error.message )
+      .then((res) => res.json())
+      .then((data) => {
+        setImages(data.images);
+        setLoading(false);
+      })
+      .catch((error) => error.message);
+  };
+
+  useEffect(() => {
+    document.title = 'Portfolio | Home';
+    getImages();
+  }, []);
+
+  if (loading) {
+    return <Loading />;
   }
 
-   useEffect(() => {
-     document.title = 'Portfolio | Home'
-     getImages()
-   }, [])
+  if (!images?.length && !loading) {
+    return null;
+  }
 
-  const randomImageIndex = Math.floor(Math.random() * images.length)
+  const randomImageIndex = Math.floor(Math.random() * images.length);
+  const image = images[randomImageIndex];
 
   return (
     <div className={styles.splashImage}>
-      {(
-        loading
-        ) ? (
-        <Loading />
-        ) : (
-        <img
-          src={ images[randomImageIndex].image_url }
-          alt={ `Image Title: ${images[randomImageIndex].image_title}` }
-        />
-      )}
+      <img src={image.image_url} alt={`Image Title: ${images.image_title}`} />
     </div>
-  )
+  );
 }
