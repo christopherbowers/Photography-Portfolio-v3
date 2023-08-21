3192986a3424dad2739c365c252b19086b686c99
christopherbowers <c_bowers@me.com>
2023-08-21 15:09:29 -0400

    Stubbed out api key protection

---
+++ api/middleware/ApiKeyMiddleware.js
@@ -0,0 +1,32 @@
+const asyncHandler = require('express-async-handler');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const ApiKeyMiddleware = asyncHandler(async (req, res, next) => {
+  let key = null;
+
+  const keys = process.env.KEYS.split(',');
+
+  if (req.query.key) {
+    key = req.query.key;
+  }
+
+  if (!key) {
+    res.status(403);
+    throw new Error('403 Forbidden');
+  }
+
+  try {
+    if (!keys.includes(key)) {
+      res.status(401);
+      throw new Error('401 Unauthorized: Invalid API key');
+    }
+
+    next();
+  } catch (error) {
+    res.status(401);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = ApiKeyMiddleware;
