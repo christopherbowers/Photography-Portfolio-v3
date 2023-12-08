7ea9a099887495199241f1cc1f9aeca1dbb64038
christopherbowers <c_bowers@me.com>
2023-12-08 10:48:26 -0500

    Removed references to heroku in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,32 +20,9 @@ const defaultConfig = {
   }
 }
 
-export default defineConfig(({ command }) => {
-  if (command === 'serve') {
-    return {
-      // dev specific config
-      ...defaultConfig
-    }
-  } else {
-    // command === 'build'
-    return {
-      // build specific config
-      ...defaultConfig,
-      server: {
-        proxy: {
-          '/api': {
-            target: 'http://bowers-portfolio.herokuapp.com/api/',
-            changeOrigin: true,
-            secure: false,
-           },
-          '/images': {
-            target: 'http://bowers-portfolio.herokuapp.com/api/images/',
-            changeOrigin: true,
-            secure: false,
-            rewrite: (path) => path.replace(/^\/images/, '')
-           },
-        }
-      }
-    }
+export default defineConfig(() => {
+  return {
+    // dev specific config
+    ...defaultConfig
   }
 })
